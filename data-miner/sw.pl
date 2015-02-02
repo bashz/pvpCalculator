41da4a9a858a95724ba3cda929eabfe2c6dbb7e1
bechir <bechir@accessnow.org>
2015-02-02 20:23:49 +0100

    including data miner script to repos

---
+++ data-miner/sw.pl
@@ -0,0 +1,202 @@
+#!/usr/bin/perl
+#
+# @File sw.pl
+# @Author bechir
+# @Created 1 févr. 2015 19:45:34
+#
+
+use strict;
+use warnings;
+use Scrappy;
+use Image::Resize;
+use Data::Dumper;
+
+my $image_path = '/home/bechir/Downloads/SwAndPo/tmpimage/';
+my $icon_path = '/home/bechir/Downloads/SwAndPo/tmpimage/icon/';
+
+my ($Swords, $Daggers, $Axes, $Harmor, $Gauntlets, $Hhelmets, $Hboots, $Armor,
+ $Boots, $Gloves, $Helmets, $Clothes, $Shoes, $Bracers, $Hats, $Shields, $Herbs,
+ $Potions, $Scrolls, $Bows, $Guns, $Thrown, $Music, $Maces, $Spears,
+ $Staves, $Amulets, $Rings, $none) = undef;
+my  $scraper = Scrappy->new;
+my  $scraper2 = Scrappy->new;
+my  $scraper3 = Scrappy->new;
+        
+if ($scraper->get('http://www.edgebee.com/wiki/index.php?title=Edgebee_Wiki:SP2_master_item_list')->page_loaded) {
+    for (my $i = 1; $i < 537; $i++){
+        my $line = undef;
+        my $elements = $scraper->select('#bodyContent table tr')->focus($i)->select('td')->data;
+        ${$elements}[0]->{html} =~ /<a href="(.*)"\s*title=/;
+        my $link = 'http://www.edgebee.com/'.$1;
+        my $pic_src = undef;
+        my $picture = clean_name(${$elements}[0]->{text}).'.png';
+        $line = ',{'.
+            '"image":"'.$picture.'",'.
+            '"Recipie":"'.remove_space(${$elements}[0]->{text}).'",'.
+            '"Category":"'.remove_space(${$elements}[1]->{text}).'",'.
+            '"Worker":"'.remove_space(${$elements}[2]->{text}).'",'.
+            '"Price":'.remove_space(${$elements}[3]->{text}).','.
+            '"Time":'.remove_space(${$elements}[4]->{text}, 1).','.
+            '"CXP":'.remove_space(${$elements}[5]->{text}).','.
+            '"SXP":'.remove_space(${$elements}[6]->{text}).','.
+            '"MaxResource":"'.remove_space(${$elements}[7]->{text}).'",'.
+            '"Rare":'.remove_space(${$elements}[8]->{text}, 1).','.
+            '"Level":'.remove_space(${$elements}[9]->{text}).','.
+            '"Workstation":"'.remove_space(${$elements}[10]->{text}).'",'.
+            '"Iron":'.remove_space(${$elements}[11]->{text}).','.
+            '"Wood":'.remove_space(${$elements}[12]->{text}).','.
+            '"Herbs":'.remove_space(${$elements}[13]->{text}).','.
+            '"Leather":'.remove_space(${$elements}[14]->{text}).','.
+            '"Fabric":"'.remove_space(${$elements}[15]->{text}).'",'.
+            '"Powder":"'.remove_space(${$elements}[16]->{text}).'",'.
+            '"Dyes":"'.remove_space(${$elements}[17]->{text}).'",'.
+            '"Elfwood":"'.remove_space(${$elements}[18]->{text}).'",'.
+            '"Glass":"'.remove_space(${$elements}[19]->{text}).'",'.
+            '"Oil":"'.remove_space(${$elements}[20]->{text}).'",'.
+            '"Ironwood":"'.remove_space(${$elements}[21]->{text}).'",'.
+            '"Steel":"'.remove_space(${$elements}[22]->{text}).'",'.
+            '"Crystals":"'.remove_space(${$elements}[23]->{text}).'",'.
+            '"Mithril":"'.remove_space(${$elements}[24]->{text}).'",'.
+            '"Gems":"'.remove_space(${$elements}[25]->{text}).'",'.
+            '"Acid":"'.remove_space(${$elements}[26]->{text}).'",'.
+            '"Tear":"'.remove_space(${$elements}[27]->{text}).'",'.
+            '"Volcanic":"'.remove_space(${$elements}[28]->{text}).'",'.
+            '"Shell":"'.remove_space(${$elements}[29]->{text}).'",'.
+            '"Feather":"'.remove_space(${$elements}[30]->{text}).'",'.
+            '"Ice":"'.remove_space(${$elements}[31]->{text}).'",'.
+            '"Horn":"'.remove_space(${$elements}[32]->{text}).'",'.
+            '"Moon":"'.remove_space(${$elements}[33]->{text}).'",'.
+            '"Roaring":"'.remove_space(${$elements}[34]->{text}).'",'.
+            '"Silk":"'.remove_space(${$elements}[35]->{text}).'",'.
+            '"Shadow":"'.remove_space(${$elements}[36]->{text}).'",'.
+            '"Dragon":"'.remove_space(${$elements}[37]->{text}).'",'.
+            '"Sun":"'.remove_space(${$elements}[38]->{text}).'",'.
+            '"Divine":"'.remove_space(${$elements}[39]->{text}).
+                '"}';
+        if(remove_space(${$elements}[1]->{text}) eq 'Amulets'){
+            $Amulets = $line. $Amulets;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Armor'){
+            $Armor = $line. $Armor;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Axes'){
+            $Axes = $line. $Axes;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Boots'){
+            $Boots = $line. $Boots;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Bows'){
+            $Bows = $line. $Bows;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Bracers'){
+            $Bracers = $line. $Bracers;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Clothes'){
+            $Clothes = $line. $Clothes;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Daggers'){
+            $Daggers = $line. $Daggers;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Gauntlets'){
+            $Gauntlets = $line. $Gauntlets;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Gloves'){
+            $Gloves = $line. $Gloves;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Guns'){
+            $Guns = $line. $Guns;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Hats'){
+            $Hats = $line. $Hats;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Heavy armor'){
+            $Harmor = $line. $Harmor;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Heavy helmets'){
+            $Hhelmets = $line. $Hhelmets;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Helmets'){
+            $Helmets = $line. $Helmets;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Herbs'){
+            $Herbs = $line. $Herbs;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Maces'){
+            $Maces = $line. $Maces;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Music'){
+            $Music = $line. $Music;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Potions'){
+            $Potions = $line. $Potions;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Rings'){
+            $Rings = $line. $Rings;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Scrolls'){
+            $Scrolls = $line. $Scrolls;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Shields'){
+            $Shields = $line. $Shields;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Shoes'){
+            $Shoes = $line. $Shoes;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Spears'){
+            $Spears = $line. $Spears;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Staves'){
+            $Staves = $line. $Staves;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Swords'){
+            $Swords = $line. $Swords;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Thrown'){
+            $Thrown = $line. $Thrown;
+        }elsif(remove_space(${$elements}[1]->{text}) eq 'Heavy boots'){
+            $Hboots = $line. $Hboots;
+        }else{
+            $none = $line. $none;
+        }
+        if ($scraper2->get($link)->page_loaded) {
+            my $src = $scraper2->select('#bodyContent table')->focus(0)->select('tr')->focus(1)->data;
+            @{$src}[-2]->{html}=~ /src="(.*)"\s/i;
+            $pic_src = $1;
+            $scraper3->download($pic_src, $image_path, $picture);
+            resize($image_path.$picture, 16, 16, $icon_path.$picture);
+        }
+        
+#print "$line\n$link\n$picture\n";
+    }
+    print "$Swords\n\n$Daggers\n\n$Axes\n\n$Harmor\n\n$Gauntlets\n\n$Hhelmets\n\n$Hboots\n\n$Armor,
+ $Boots\n\n$Gloves\n\n$Helmets\n\n$Clothes\n\n$Shoes\n\n$Bracers\n\n$Hats\n\n$Shields\n\n$Herbs,
+ $Potions\n\n$Scrolls\n\n$Bows\n\n$Guns\n\n$Thrown\n\n$Music\n\n$Maces\n\n$Spears,
+ $Staves\n\n$Amulets\n\n$Rings\n\n$none";
+}
+sub clean_name{
+    my $clean = shift @_;
+    $clean =~ s/\s/-/g;
+    $clean =~ s/\*//;
+    return $clean;
+}
+sub remove_space{
+    my $unspaced = shift @_;
+    $unspaced =~ s/^ //;
+    $unspaced =~ s/ $//;
+    if($_[0]){
+        if($unspaced eq 'No'){
+            return 0;
+        }elsif($unspaced eq 'Very Fast' || $unspaced eq 'Yes'){
+            return 1;
+        }elsif($unspaced eq 'Fast'){
+            return 2;
+        }elsif($unspaced eq 'Medium'){
+            return 3;
+        }elsif($unspaced eq 'Long'){
+            return 4;
+        }elsif($unspaced eq 'Very Long'){
+            return 5;
+        }else{
+            return "unspec";
+        }
+    }
+    return $unspaced;
+}
+sub resize {
+    my ($inputfile, $width, $height, $outputfile) = @_;
+    GD::Image->trueColor(1);
+    my $gdo = GD::Image->new($inputfile);
+
+    {
+        my $k_h = $height / $gdo->height;
+        my $k_w = $width / $gdo->width;
+        my $k   = ($k_h < $k_w ? $k_h : $k_w);
+        $height = int($gdo->height * $k);
+        $width  = int($gdo->width * $k);
+    }
+
+    my $image = GD::Image->new($width, $height);
+    $image->alphaBlending(0);
+    $image->saveAlpha(1);
+    $image->copyResampled($gdo, 0, 0, 0, 0, $width, $height, $gdo->width, $gdo->height);
+
+    open my $FH, '>', $outputfile;
+    binmode $FH;
+    print {$FH} $image->png;
+    close $FH;
+}
+__END__
